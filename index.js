e72c84c5327e2ee35d92290e8814ffcda4f5a09a
Carmelo Martinez-Vazquez <Carmelo.M.Vazquez@hotmail.com>
2022-02-28 14:08:40 -0500

    commit tree d995a39e8efb565ea3cd597d91940d0cd3274a50

---
+++ index.js
@@ -0,0 +1,12 @@
+function sayHelloTo(firstName = "User") {
+    console.log('Hello, ${firstname}!');
+}
+function introduction(name){
+    return'Hi, my name is Samip.'
+}
+function introductionWithLanguage(name, language){
+    return 'Hi, my name is ${name} and I am learning to program in ${language}.'
+}
+function introductionWithLanguageWithLanguageOptional(name, language = "JavaScript"){
+
+}
